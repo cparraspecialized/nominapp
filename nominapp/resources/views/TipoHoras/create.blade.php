828ddd09ce48385d5514d9119978764a42b86874
briam-spe <briam.bermudez@specialized.com>
2020-03-27 15:19:12 -0500

    Vista tipo hora

--- nominapp/resources/views/TipoHoras/create.blade.php
+++ nominapp/resources/views/TipoHoras/create.blade.php
@@ -1,21 +1,23 @@
 @extends ('layouts.admin')
 @section ('content')
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h3>Crear un tipo de hora</h3>
-           
         {!!Form::open(array('url'=>'TipoHoras','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
-        {{Form::token()}}        
-        <h4><div class="form-group">
-            <label for="descripcionTipo">Descripcion tipo hora</label>
-            <input type="text" name="descripcionTipo" class="form-control" placeholder="Descripcion">
-        </div>
-        <div class="form-group">
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
-            <button class="btn btn-danger btn-lg btn-block" type="reset">Cancelar</button>
-        </div></h4>
-
-        {!!Form::close()!!}
-        </div>
-   
+        {{Form::token()}}
+     
+          <div class="col-lg-12 col-sm-12 form-group">
+            <div class="card card-primary card-outline">
+              <div class="card-header">
+                <h5 class="card-title m-0">Crear un tipo de hora</h5>
+              </div>
+              <div class="card-body">               
+                <div class="form-group">
+                    <label for="descripcionTipo"><p class="font-weight-normal">Descripcion tipo hora: </p></label>
+                    <input type="text" name="descripcionTipo" class="form-control" placeholder="Descripcion">
+                </div>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
+                <button class="btn btn-danger btn-lg btn-block" type="reset">Cancelar</button>
+    
+              </div>
+            </div>
+          </div>
+          {!!Form::close()!!}
 @endsection
