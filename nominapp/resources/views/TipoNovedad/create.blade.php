677e2945f850dc0052cbca8673227c06e65c1e72
briam-spe <briam.bermudez@specialized.com>
2020-03-27 15:22:00 -0500

    Vista tipo novedad

--- nominapp/resources/views/TipoNovedad/create.blade.php
+++ nominapp/resources/views/TipoNovedad/create.blade.php
@@ -1,22 +1,23 @@
 @extends ('layouts.admin')
 @section ('content')
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h3>Crear Un Tipo de Novedad</h3>
-           
-        {!!Form::open(array('url'=>'TipoNovedad','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
+    {!!Form::open(array('url'=>'TipoHoras','method'=>'POST','autocomplete'=>'off','files'=>'true'))!!}
         {{Form::token()}}
-        <h4><div class="form-group">
-            <label for="descripcionTipoNovedad">Descripcion Novedad:</label>
-            <input type="text" name="descripcionTipoNovedad" class="form-control" placeholder="Descripcion tipo novedad">
-        </div>
-
-        <div class="form-group">
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
-            <button class="btn btn-danger btn-lg btn-block" type="reset">Cancelar</button>
-        </div></h4>
-
-        {!!Form::close()!!}
-    </div>
-
+     
+          <div class="col-lg-12 col-sm-12 form-group">
+            <div class="card card-primary card-outline">
+              <div class="card-header">
+                <h5 class="card-title m-0">Crear Un Tipo de Novedad</h5>
+              </div>
+              <div class="card-body">               
+                <div class="form-group">
+                    <label for="descripcionTipoNovedad"><p class="font-weight-normal">Descripcion Novedad: </p></label>
+                    <input type="text" name="descripcionTipoNovedad" class="form-control" placeholder="Descripcion tipo novedad">
+                </div>
+                <button class="btn btn-primary btn-lg btn-block" type="submit">Guardar</button>
+                <button class="btn btn-danger btn-lg btn-block" type="reset">Cancelar</button>
+    
+              </div>
+            </div>
+          </div>
+          {!!Form::close()!!}
 @endsection
