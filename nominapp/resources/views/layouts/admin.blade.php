0db3280850b31f0badfdc6089d8675ad31a6e3f7
briam-spe <briam.bermudez@specialized.com>
2020-04-27 15:43:32 -0500

    no message

--- nominapp/resources/views/layouts/admin.blade.php
+++ nominapp/resources/views/layouts/admin.blade.php
@@ -69,7 +69,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar elevation-4 sidebar-dark-indigo">
     <!-- Brand Logo -->
-    <a  class="brand-link navbar-purple">
+    <a href="#" class="brand-link navbar-purple">
       <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
