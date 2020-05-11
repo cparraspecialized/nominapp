fc331722ebba2b15c331f8da259b9b700e9c9022
briam-spe <briam.bermudez@specialized.com>
2020-05-11 09:58:03 -0500

    Modifcacion botton de mayusculas a minusculas

--- nominapp/resources/views/TipoCargos/search.blade.php
+++ nominapp/resources/views/TipoCargos/search.blade.php
@@ -14,7 +14,7 @@
               <button class="btn btn-outline-primary btn-block  form-group"  type="submit">Buscar</button>
             </div> 
             <div class="col-sm-4 form-group">             
-              <a href="{{route('TipoCargos.create')}}"> <button type="button" class="btn btn-outline-success btn-block">CREAR</button>                        
+              <a href="{{route('TipoCargos.create')}}"> <button type="button" class="btn btn-outline-success btn-block">Crear</button>                        
             </div>
 
             </div>  
